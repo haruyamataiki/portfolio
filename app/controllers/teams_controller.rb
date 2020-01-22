a0db6f32e65f4aa89990b5a4d27121bd8b97bc76
haruyamataiki <haruyamataiki619@gmali.com>
2020-01-22 07:58:46 +0000

    update_style

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -12,22 +12,36 @@ class TeamsController < ApplicationController
   def new
   	 # if current_customer.teams.find_by(team_id: params[:team_id]).empty?
  	if Team.find_by(customer_id: current_customer.id).present?
-    redirect_to customers_path
-     else
+     redirect_to customers_path
+  else
      @team = Team.new
      end
   end
 
   def create
-    @team = Team.new(team_params)
-    @team.customer_id = current_customer.id
-    @team.build_room
-	 if @team.save
-        redirect_to teams_path,notice:"successfully"
-    else
-        flash[:notice] = "error"
-	    redirect_to customers_path
-    end
+     @team = Team.new(team_params)
+     @team.customer_id = current_customer.id
+     @team.build_room
+	if @team.save
+     redirect_to teams_path,notice:"successfully"
+  else
+     flash[:notice] = "error"
+	   redirect_to teams_path
+     end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+     @team = Team.find(params[:id])
+  if @team.update(team_params)
+     redirect_to team_path(@team.id),notice:"successfully"
+  else
+     flash[:notice] = "error"
+     ender :edit
+     end
   end
 
 private
