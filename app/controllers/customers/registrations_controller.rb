18b92aa8bcb70e1e2a9eca2f2cb081f053a3db1d
haruyamataiki <haruyamataiki619@gmali.com>
2020-02-03 10:09:00 +0000

    Update:devise

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -20,7 +20,6 @@ class Customers::RegistrationsController < Devise::RegistrationsController
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
-        ThanksMailer.send_mail(current_customer).deliver_now
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
